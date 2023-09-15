fdabd541154450141183e6c5ac6967b2832e3a96
Hettal Adel <120855016+adelhettal@users.noreply.github.com>
2023-09-15 22:25:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// String primitive
+const message = 'hi';
+// String object
+const another = new String('hi');
+
+const another = 'This is my
+first message' 
+
+const message =
+'This is my\n'
++
+"'first\' message';
+const another =
+"Hi ${name}.
+Thank you for joining my mailing list.
+Regards,
+Mosh';
+const now = new Date() ;
+const datel = new Date( 'May 11 2018 09:00");
+const date2 = new Date (2018, 4, 11, 9);
